9d9cc407941b32df174b607e0ec3c0133387591d
ברנשטיין מרים לאה <  @mbjcomp.local>
2024-02-13 15:15:53 +0200

    adding connection string

--- Server/Server/Server/Program.cs
+++ Server/Server/Server/Program.cs
@@ -1,6 +1,14 @@
+using Dal.Models;
+using Dal;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+Dbactions actions = new Dbactions(builder.Configuration);
+var connection = actions.GetConnectionString("NotnimYadDB");
+builder.Services.AddDbContext<NotnimYadContext>(options => options.UseSqlServer(connection));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();
