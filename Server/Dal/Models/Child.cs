b704a09169f3bf009ed750361c56844ef5855843
Your Name <you@example.com>
2024-02-20 14:21:23 +0200

    Another run of scaffold

--- Server/Dal/Models/Child.cs
+++ Server/Dal/Models/Child.cs
@@ -22,4 +22,10 @@ public partial class Child
     public int AddressId { get; set; }
 
     public string Comments { get; set; }
+
+    public virtual Address Address { get; set; }
+
+    public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
 }
